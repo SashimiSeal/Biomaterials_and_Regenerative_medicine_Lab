3d4a276961472fe98a589509ef98ecebe89925bc
Hana <taco.tiger421@gmail.com>
2025-07-10 12:18:58 -0400

    transfer from vsc

---
+++ script.js
@@ -0,0 +1,18 @@
+// let count = 0;
+// const max = 4;
+
+// const intervalId = setInterval(() => {
+
+//     let val = count % 4;
+//     console.log(val);
+//     console.log(fruits[val]);
+//     count++;
+    
+//   }, 1000);
+
+// const fruits = [
+//     'https://picsum.photos/id/21/200/300', 
+//     'https://picsum.photos/id/14/200/300', 
+//     'https://picsum.photos/id/33/200/300', 
+//     'https://picsum.photos/id/1/200/300'
+// ];
